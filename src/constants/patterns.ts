5e099b316f9b1f6b94cf17a83c36246c973cfd1b
Rachel <rachelreillydev@gmail.com>
2022-02-13 15:39:47 -0600

    adds stitch pattenr

--- src/constants/patterns.ts
+++ src/constants/patterns.ts
@@ -2,11 +2,23 @@ import type { PatternType } from '../types'
 
 export const PATTERNS: readonly PatternType[] = [
   {
+    name: 'current',
+    text: 'dark'
+  },
+  {
     name: 'ring',
     text: 'dark'
   },
   {
     name: 'velvet',
+    text: 'light'
+  },
+  {
+    name: 'stitch',
+    text: 'light'
+  },
+  {
+    name: 'example',
     text: 'light'
   }
 ] as const
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         ring: "url('/src/svg/ring.svg')",
-        velvet: "url('/src/svg/velvet.svg')"
+        velvet: "url('/src/svg/velvet.svg')",
+        stitch: "url('/src/svg/velvet.svg')",
+        example: "url('/src/svg/example.svg')",
+        current: "url('/src/svg/current.svg')"
       }
     }
   }
